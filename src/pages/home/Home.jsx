df5daaa4e269a1ed0a21766c6d6f468c89c36dfa
geilsonfreire <geilsonfreireleite@gmail.com>
2024-06-16 17:47:10 -0300

    Foi implementado a home page jsx e css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,16 +4,34 @@ import React from 'react'
 // importa css
 import './Home.css'
 
+// Imports Icon, IMG, Assets 
+import Hero_Banner from '../../assets/img/hero_banner.jpg'
+import Hero_Title from '../../assets/img/hero_title.png'
+import { BsPlayFill, BsInfoCircle } from "react-icons/bs";
+
 // Imports Components
 import Navbar from '../../components/navbar/Navbar'
 
 const Home = () => {
-  return (
-    <div className='home'>
-      <Navbar/>
-      
-    </div>
-  )
+    return (
+        <div className='home'>
+            <Navbar />
+            <div className="hero">
+                <img className='banner-img' src={Hero_Banner} alt="Banner" />
+                <div className="hero-caption">
+                    <img className='caption-img' src={Hero_Title} alt="Banner titulo" />
+                    <p className='caption-text'>
+                        Depois de descobrir os seus laços com uma antiga ordem secreta, um homem que vive em Istambul embarca numa missão para salvar a cidade de um inimigo imortal.
+                    </p>
+
+                    <div className="hero-btns">
+                        <button className='btn'><i><BsPlayFill /></i>Assistir</button>
+                        <button className='btn btn-info'><i><BsInfoCircle /></i>Mais  informações</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
