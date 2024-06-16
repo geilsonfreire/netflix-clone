5b035b64cfd0530e0234ba4405d5e1dca96fc4e2
geilsonfreire <geilsonfreireleite@gmail.com>
2024-06-16 16:32:37 -0300

    Foi criado o menu de navegaçao

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,13 +5,16 @@ import React from 'react'
 import './Navbar.css'
 
 // Imports icon, img, assets
-import logo from '../../assets/logo.png'
+import logo from '../../assets/img/logo.png'
+import Profile_img from '../../assets/img/profile_img.png'
+import { BsSearch, BsBell, BsCaretDown } from "react-icons/bs";
 
 const Navbar = () => {
     return (
         <div className='navbar'>
             <div className="navbar-left">
                 <img src={logo} alt="logo" />
+
                 <ul>
                     <li>Inicio</li>
                     <li>Séries</li>
@@ -21,7 +24,21 @@ const Navbar = () => {
                     <li>Navegar por idiomas</li>
                 </ul>
             </div>
-            <div className="navbar-right"></div>
+            <div className="navbar-right">
+                <i className='icons'>{<BsSearch />}</i>
+                <p>Infantil</p>
+                <i className='icons'>{<BsBell />}</i>
+
+                <div className="navbar-profile">
+                    <img className='profile' src={Profile_img} alt="Perfil" />
+                    <i className='navbar-profile'>{<BsCaretDown />}</i>
+
+                    <div className="dropdown">
+                        <p>Sair da Netflix</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
