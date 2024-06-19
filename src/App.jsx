ddf6bad9da438e288e6a121fef2e4a6cc7c047d9
geilsonfreire <geilsonfreireleite@gmail.com>
2024-06-18 21:55:09 -0300

    Foi instalado a biblioteca react-route-dom e implementado as rotas entre pages home e login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 // Imports Bibliotecas
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 // Imports Components
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 
 const App = () => {
-  return (
-    <div>
-      <Home/>
-    </div>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+// Import Bibliotecas
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+// Import css
 import './index.css'
 
+// Import Componentes
+import App from './App.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
 )
