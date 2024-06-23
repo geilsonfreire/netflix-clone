f0016003391c4fdcd1ab4337281ea52b33b6a7c4
geilsonfreire <geilsonfreireleite@gmail.com>
2024-06-23 11:15:26 -0300

    feat: Ajustar alinhamento do ícone de perfil na barra de navegação
    Corrige o alinhamento do ícone de perfil na barra de navegação para exibição correta. Agora o ícone é alinhado corretamente com o texto no menu suspenso.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,32 +1,33 @@
 // Imports Bibliotecas
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify'; 
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { toast, ToastContainer } from 'react-toastify';
+
 
 // Importa CSS
 import './Login.css'
 import 'react-toastify/dist/ReactToastify.css'; // Importa o CSS do Toastify
 
 // Import Icons, img, Assets
-import Logo from '../../assets/img/logo.png'
+import Logo from '../../assets/img/logo.png';
 
 
 // Import Components
 import Footer from '../../components/footer/Footer'
-import { login } from '../../Firebase/FirebaseAuth'
+import { login } from '../../Firebase/FirebaseAuth';
 
 
 const Login = () => {
     const [email, setEmail] = useState(''); // Estado inicial do email
     const [password, setPassword] = useState(''); // Estado inicial da senha
-    const Navigate = useNavigate();
-    const auth = getAuth();
-    
+    const Navigate = useNavigate(); // Função de navegação
+
+
 
     const user_auth = async (e) => {
         e.preventDefault();
-
+        
+        
         // Verifica se os campos de email e senha estão preenchidos
         if (!email || !password) {
             toast.error('Por favor, preencha todos os campos.');
@@ -39,54 +40,58 @@ const Login = () => {
                 toast.success('Logado com sucesso!');
                 Navigate('/'); // Redireciona para a Home
             } else {
+             
                 toast.error('Erro ao fazer login. Tente novamente.');
             }
         } catch (error) {
             toast.error(error.message);
+           
         }
-        
+       
+
     }
 
     return (
-        <div className='login'>
-            <ToastContainer /> {/* Adiciona o container para exibir as mensagens */}
-            <img className='login-logo' src={Logo} alt="Logo" />
-            <div className="login-form">
-                <h1>Login</h1>
-                <form onSubmit={user_auth}>
-                    <input type="email" placeholder='Email' 
-                        value={email}
-                        onChange={(e) => {setEmail(e.target.value)}}
-                    />
+        
+            <div className='login'>
+                <img className='login-logo' src={Logo} alt="Logo" />
+                <div className="login-form">
+                    <h1>Login</h1>
+                    <form onSubmit={user_auth}>
+                        <input type="email" placeholder='Email'
+                            value={email}
+                            onChange={(e) => { setEmail(e.target.value) }}
+                        />
 
-                    <input type="password" placeholder="Senha" 
-                        value={password}
-                        onChange={(e) =>{ setPassword(e.target.value)}}
-                    />
+                        <input type="password" placeholder="Senha"
+                            value={password}
+                            onChange={(e) => { setPassword(e.target.value) }}
+                        />
 
-                    <div className="login-buttons">
-                        <button type="submit">Entrar</button>
-                        <span className='OU'>OU</span>
-                        <button className='codigo-acesso' type="submit">Usar um código de acesso</button>
-                    </div>
-
-                    <div className="form-help">
-                        <a href="#">Esqueceu a senha?</a>
-
-                        <div className="lembrar">
-                            <input type="checkbox" />
-                            <label>Lembre-se de mim</label>
+                        <div className="login-buttons">
+                            <button type="submit">Entrar</button>
+                            <span className='OU'>OU</span>
+                            <button className='codigo-acesso' type="submit">Usar um código de acesso</button>
                         </div>
-                        
-                        <p>
-                            Novo por aqui? 
-                            <Link to="/AssineNetflix">Assine agora.</Link>
-                        </p>
-                    </div>
-                </form>
+
+                        <div className="form-help">
+                            <a href="#">Esqueceu a senha?</a>
+
+                            <div className="lembrar">
+                                <input type="checkbox" />
+                                <label>Lembre-se de mim</label>
+                            </div>
+
+                            <p>
+                                Novo por aqui?
+                                <Link to="/AssineNetflix">Assine agora.</Link>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+                <Footer />
+                <ToastContainer />
             </div>
-            <Footer/>
-        </div>
     )
 }
 
